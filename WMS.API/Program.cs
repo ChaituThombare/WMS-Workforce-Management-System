8bb3f446872d74b0bf8c8b86ce0ce3ad3b165358
chaitu <chaitannyathomabre@gmail.com>
2026-05-27 16:41:20 +0530

    Fix CORS for deployed frontend

--- WMS.API/Program.cs
+++ WMS.API/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowAngular",
         policy =>
         {
-            policy.WithOrigins("http://localhost:4200")
+            policy.WithOrigins("http://localhost:4200", "https://mango-ground-0ca652800.7.azurestaticapps.net")
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
